a6ebf6e95fc81731d3acec25e603ce2fc537e406
BuildTools <unconfigured@null.spigotmc.org>
2022-03-29 19:41:15 -0400

    More

--- main.go
+++ main.go
@@ -27,7 +27,27 @@ func main() {
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
+		_, err := fmt.Scanln(&originValue)
+
+		if err != nil {
+			printError(errReadingInput)
+		}
+
+		if originUnit == "C" {
+			convertToFahrenheit(originValue)
+		} else {
+			convertToCelsius(originValue)
+		}
+
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+
+		_, err = fmt.Scanln(&shouldConvertAgain)
+
+		if err != nil {
+			printError(errReadingInput)
+		}
+
+		shouldConvertAgain = strings.TrimSpace(strings.ToUpper(shouldConvertAgain))
 
 		if shouldConvertAgain != "Y" {
 			fmt.Println("Good bye!")
